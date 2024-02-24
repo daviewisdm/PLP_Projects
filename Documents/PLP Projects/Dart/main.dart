55fc361c654d801bba0bfa7c7611797e0c210435
Davie Kibet <wisdomdavie24@gmail.com>
2024-02-24 17:47:03 +0300

    .\main.dart

---
+++ Documents/PLP Projects/Dart/main.dart
@@ -0,0 +1,14 @@
+
+void main () {
+  //Devlare variables
+  String name = "Davie";
+  num age = 20;
+  num weight = 56.7;
+  bool isMale = true;
+  //Print the variables
+  print("My name is $name");
+  print("Age is $age years old");
+  print("Weight is $weight kg");
+  print("Marriage status is married : $isMale");
+
+}
