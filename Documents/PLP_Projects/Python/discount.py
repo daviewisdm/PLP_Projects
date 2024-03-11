76b4f8fd303b236fa60be7db6df1faf27d7d1159
Davie Kibet <wisdomdavie24@gmail.com>
2024-03-12 00:26:50 +0300

    Push Py

---
+++ Documents/PLP_Projects/Python/discount.py
@@ -0,0 +1,11 @@
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        final_price = price - (price * (discount_percent / 100))
+        return final_price
+    else:
+        return price
+
+original_price = float(input("Enter the original price of the item: "))
+discount_percentage = float(input("Enter the discount percentage: "))
+final_price = calculate_discount(original_price, discount_percentage)
+print("The final price after applying the discount is:", final_price)
